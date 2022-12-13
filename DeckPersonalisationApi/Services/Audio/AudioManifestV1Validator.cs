8afefa3464d4f0b294c5173d203187ff52c3e8fd
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2022-12-13 01:15:06 +0100

    Add audioloader manifest validators

---
+++ DeckPersonalisationApi/Services/Audio/AudioManifestV1Validator.cs
@@ -0,0 +1,108 @@
+﻿using DeckPersonalisationApi.Model;
+using Newtonsoft.Json.Linq;
+
+namespace DeckPersonalisationApi.Services.Audio;
+
+public class AudioManifestV1Validator
+{
+    protected string _packPath;
+    protected JObject _json;
+    protected List<string> _validFiles;
+    
+    public string Name { get; protected set; }
+    public string Description { get; protected set; } = "";
+    public string Author { get; protected set; }
+    public string Version { get; protected set; } = "v1.0";
+    public int ManifestVersion { get; protected set; }
+    public bool Music { get; protected set; }
+    public List<string> Ignore { get; protected set; } = new();
+    public Dictionary<string, List<string>> Mappings { get; protected set; } = new();
+    
+    public AudioManifestV1Validator(string packPath, JObject json, User user, List<string> validFiles)
+    {
+        _packPath = packPath;
+        _json = json;
+        Author = user.Username;
+        _validFiles = validFiles;
+        ManifestVersion = 1;
+    }
+    
+    protected virtual void VerifyName()
+    {
+        if (!_json.ContainsKey("name"))
+            throw new Exception("No name was found");
+
+        Name = _json["name"]!.ToObject<string>()!;
+    }
+
+    protected virtual void VerifyAuthor()
+    {
+        if (_json.ContainsKey("author"))
+            Author = _json["author"]!.ToObject<string>()!;
+        else
+            _json["author"] = Author;
+    }
+
+    protected virtual void VerifyVersion()
+    {
+        if (_json.ContainsKey("version"))
+            Version = _json["version"]!.ToObject<string>()!;
+    }
+    
+    protected virtual void VerifyMusicBool()
+    {
+        if (_json.ContainsKey("music"))
+            Music = _json["music"]!.ToObject<bool>()!;
+    }
+    
+    protected virtual void VerifyDescription()
+    {
+        if (_json.ContainsKey("description"))
+        {
+            Description = _json["description"]!.ToObject<string>()!;
+            _json.Remove("description");
+        }
+    }
+
+    protected virtual void VerifyMappings()
+    {
+        if (_json.ContainsKey("mappings"))
+            throw new Exception("Mappings are not supported by v1 manifests");
+    }
+    
+    protected virtual void CalculateIgnores()
+    {
+        foreach (var validFile in _validFiles)
+        {
+            string path = Path.Join(_packPath, validFile);
+
+            if (Mappings.ContainsKey(validFile) && Path.Exists(path))
+                throw new Exception($"{validFile} is found in both mappings and path");
+            
+            if (!Mappings.ContainsKey(validFile) && !Path.Exists(path))
+                Ignore.Add(validFile);
+        }
+
+        if (_json.ContainsKey("ignore"))
+            _json.Remove("ignore");
+        
+        _json.Add("ignore", new JArray(Ignore.ToArray()));
+    }
+    
+    public virtual void FullVerify()
+    {
+        VerifyName();
+        VerifyAuthor();
+        VerifyVersion();
+        VerifyDescription();
+
+        if (Music)
+            _validFiles = new() { "menu_music.mp3" };
+        
+        VerifyMappings();
+        CalculateIgnores();
+
+        if (Music && Ignore.Count >= 1)
+            throw new Exception("menu_music.mp3 is not present and is required for a music pack");
+    }
+}
---
+++ DeckPersonalisationApi/Services/Audio/AudioManifestV2Validator.cs
@@ -0,0 +1,46 @@
+﻿using DeckPersonalisationApi.Model;
+using Newtonsoft.Json.Linq;
+
+namespace DeckPersonalisationApi.Services.Audio;
+
+public class AudioManifestV2Validator : AudioManifestV1Validator
+{
+    public AudioManifestV2Validator(string packPath, JObject json, User user, List<string> validFiles) : base(packPath, json, user, validFiles)
+    {
+        ManifestVersion = 2;
+    }
+
+    protected override void VerifyMappings()
+    {
+        if (!_json.ContainsKey("mappings"))
+            return;
+        
+        if (_json["mappings"] is not JObject patches)
+            throw new Exception("Patches section of pack.json is not a dictionary");
+        
+        foreach (var (key, value) in patches)
+        {
+            if (value is not JArray files)
+                throw new Exception("Mappings item is not of type array");
+
+            if (!_validFiles.Contains(key))
+                throw new Exception($"{key} in mappings is not a valid sound override");
+            
+            Mappings.Add(key, new());
+
+            foreach (var jToken in files)
+            {
+                string file = jToken.ToObject<string>()!;
+                string path = Path.Join(_packPath, file);
+
+                if (!File.Exists(path))
+                    throw new Exception($"File {file} does not exist");
+                
+                Mappings[key].Add(file);
+            }
+
+            if (Mappings[key].Count <= 0)
+                throw new Exception("Empty mapping");
+        }
+    }
+}
