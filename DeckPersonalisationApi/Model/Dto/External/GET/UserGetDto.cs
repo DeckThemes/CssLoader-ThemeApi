51050040fa162ed69b6f0da21ebe6c1547c9571d
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-01-14 21:37:20 +0100

    How dare you to ask me to rename a variable beebles

--- DeckPersonalisationApi/Model/Dto/External/GET/UserGetDto.cs
+++ DeckPersonalisationApi/Model/Dto/External/GET/UserGetDto.cs
@@ -8,7 +8,7 @@ public class UserGetDto
     public List<string> Permissions { get; set; }
     public DateTimeOffset LastLoginDate { get; set; }
     public bool Active { get; set; }
-    public bool HasApiKey { get; set; }
+    public bool HasDeckKey { get; set; }
 
     public UserGetDto(User user)
     {
@@ -18,6 +18,6 @@ public class UserGetDto
         LastLoginDate = user.LastLoginDate;
         Active = user.Active;
         Avatar = user.GetAvatarUri();
-        HasApiKey = user.ApiToken != null;
+        HasDeckKey = user.ApiToken != null;
     }
 }
