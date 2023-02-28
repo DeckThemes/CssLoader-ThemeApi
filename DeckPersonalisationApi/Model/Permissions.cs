bf43520058054b35de15ca46df4c79d6a7977e9d
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-02-28 23:40:33 +0100

    Remove unused permissions

--- DeckPersonalisationApi/Model/Permissions.cs
+++ DeckPersonalisationApi/Model/Permissions.cs
@@ -10,11 +10,6 @@ public enum Permissions
     ManageApi = 0x4,
     FromApiToken = 0x8,
     ViewThemeSubmissions = 0x10,
-    
-    PremiumTier1 = 0x100,
-    PremiumTier2 = 0x200,
-    PremiumTier3 = 0x400,
-    //PremiumTier4 = 0x800,
 }
 
 public static class PermissionExt
@@ -34,16 +29,7 @@ public static class PermissionExt
         
         if (permissions.HasPermission(Permissions.ManageApi))
             perms.Add(Permissions.ManageApi.ToString());
-        
-        if (permissions.HasPermission(Permissions.PremiumTier1))
-            perms.Add(Permissions.PremiumTier1.ToString());
-        
-        if (permissions.HasPermission(Permissions.PremiumTier2))
-            perms.Add(Permissions.PremiumTier2.ToString());
-        
-        if (permissions.HasPermission(Permissions.PremiumTier3))
-            perms.Add(Permissions.PremiumTier3.ToString());
-            
+
         return perms;
     }
 
